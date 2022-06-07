3b56eb8a9444ae150f5fbf18702528d9a08d9ba0
Gia <gia.jihye.j@gmail.com>
2022-06-07 17:24:08 +0900

    simple-diary에 ts 적용하기

---
+++ react/simple-diary-tx/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <DiaryEditor/>
+    </div>
+  );
+}
+
+export default App;
